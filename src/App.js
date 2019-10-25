0c92cdea765041e96d68e3b59acb2c84b815428c
Halvor Lund <halvor.lund@gmail.com>
2019-10-25 19:26:41 +0200

    Print road reference

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { geolocated } from 'react-geolocated';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HttpError extends Error {
+    constructor(response) {
+        super(`${response.status} for ${response.url}`);
+        this.name = 'HttpError';
+        this.response = response;
+    }
 }
 
-export default App;
+class App extends React.Component {
+
+    state = {
+        position: null,
+        error: null
+    }
+
+    componentDidMount() {
+    }
+
+    componentDidUpdate(prevProps) {
+        if(this.props.coords !== prevProps.coords) {
+            console.log(this.props.coords);
+            fetch(`https://www.vegvesen.no/nvdb/api/v2/posisjon?lat=${this.props.coords.latitude}&lon=${this.props.coords.longitude}&maks_avstand=100`)
+                .then(res => res.json())
+                .then((data) => {
+                    this.setState({ position: data[0] })
+                })
+                .catch(console.log);
+        }
+    }
+
+    render () {
+    return (
+            <div className="App">
+            <h1>Road reference app</h1>
+            {this.state.position && this.state.position.vegreferanse.kortform}
+        </div>
+    );
+    }
+}
+
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+})(App);
