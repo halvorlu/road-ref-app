fc94a7b22fc50417170f9bf1ca9042beb2b2f7b9
Halvor Lund <halvor.lund@gmail.com>
2019-10-26 11:50:55 +0200

    Prettier HTML

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ class App extends React.Component {
             latitude: trp.location.coordinates.latLon.lat,
             longitude: trp.location.coordinates.latLon.lon
           })
-        }
+        };
       });
       const closestTrp = trpsWithDistance.reduce((result, obj) => {
         return (result.distance < obj.distance) ? result : obj;
@@ -221,8 +221,8 @@ class App extends React.Component {
       this.lastUpdate = now;
       this.onNewTrps(this.state.trps);
       fetch(
-          `https://www.vegvesen.no/nvdb/api/v2/posisjon?lat=${this.props.coords.latitude}&lon=${this.props.coords.longitude}&maks_avstand=200`
-        )
+        `https://www.vegvesen.no/nvdb/api/v2/posisjon?lat=${this.props.coords.latitude}&lon=${this.props.coords.longitude}&maks_avstand=200`
+      )
         .then(res => {
           if (res.ok) {
             return res.json();
@@ -256,49 +256,53 @@ class App extends React.Component {
     const trp = this.state.closestTrp;
     return (
       <div className="App">
-            <h2>Nærmeste vegreferanse</h2>
-            <p>
-            {this.state.error}
-        </p>
-            <p>
-            Vegreferanse: {this.state.position && this.state.position.vegreferanse.kortform}
-        </p>
-            <p>
-            Avstand: {this.state.position && this.state.position.avstand}m
+        <h2>Nærmeste vegreferanse</h2>
+        <table className="center">
+          <tr>
+            <td></td><td>{this.state.error}</td>
+          </tr>
+          <tr>
+            <td>Vegreferanse:</td><td>{this.state.position && this.state.position.vegreferanse.kortform}</td>
+          </tr>
+          <tr>
+            <td>Avstand: </td><td>{this.state.position && this.state.position.avstand}m
         {this.props.coords && (<span> +/- {Math.round(this.props.coords.accuracy)}m</span>)}
-        </p>
-            <p>
-            Posisjon sist oppdatert: {moment(this.lastUpdate).format('YYYY-MM-DD hh:mm:ss')}
-        </p>
-            <h2>Nærmeste TRP</h2>
-            <p>
-            Navn: {trp ?
-                  (<a href={`http://www.vegvesen.no/trafikkdata/start/kart?trpids=${trp.id}&lat=${trp.location.coordinates.latLon.lat}&lon=${trp.location.coordinates.latLon.lon}&zoom=13`}>{trp.name}</a>) : ""}
-        </p>
-            <p>
-            Vegreferanse for TRP: {trp && trp.location.roadReference.shortForm}
-            </p>
-            <p>
-            Avstand: {this.state.closestTrpDistance}m
-        </p>
-<p>
-Siste ÅDT: {trp && this.state.ydt ?
-(<span><a href={`https://www.vegvesen.no/trafikkdata/start/utforsk?datatype=averageDailyYearVolume&display=chart&trpids=${trp.id}`}>{this.state.ydt.total.volume.average}</a> ({this.state.ydt.year}, {Math.round(this.state.ydt.total.coverage.percentage)}% dekningsgrad)</span>)
- : ""}
-</p>
-            <p>
-            Trafikk siste dag: {trp && this.state.dt ?
-                                this.renderDayTraffic()
-                                : ""}
-        </p>
-            </div>
+            </td>
+          </tr>
+          <tr>
+            <td>Posisjon sist oppdatert:</td><td>{moment(this.lastUpdate).format('YYYY-MM-DD hh:mm:ss')}</td>
+          </tr>
+        </table>
+        <h2>Nærmeste TRP</h2>
+        <table className="center">
+        <tr>
+          <td>Navn:</td><td>{trp ?
+            (<a href={`http://www.vegvesen.no/trafikkdata/start/kart?trpids=${trp.id}&lat=${trp.location.coordinates.latLon.lat}&lon=${trp.location.coordinates.latLon.lon}&zoom=13`}>{trp.name}</a>) : ""}</td>
+        </tr>
+        <tr>
+          <td>Vegreferanse:</td><td>{trp && trp.location.roadReference.shortForm}</td>
+        </tr>
+        <tr>
+          <td>Avstand:</td><td>{this.state.closestTrpDistance}m</td>
+        </tr>
+        <tr>
+          <td>Siste ÅDT:</td><td>{trp && this.state.ydt ?
+            (<span><a href={`https://www.vegvesen.no/trafikkdata/start/utforsk?datatype=averageDailyYearVolume&display=chart&trpids=${trp.id}`}>{this.state.ydt.total.volume.average}</a> ({this.state.ydt.year}, {Math.round(this.state.ydt.total.coverage.percentage)}% dekningsgrad)</span>)
+            : ""}</td>
+        </tr>
+        <tr>
+          <td>Trafikk siste dag:</td><td>{trp && this.state.dt ?
+            this.renderDayTraffic()
+            : ""}</td>
+        </tr>
+        </table>
+      </div>
     );
   }
 
   renderDayTraffic() {
     const trp = this.state.closestTrp;
     const fromDate = this.state.dt.from.split('T')[0];
-
     return (
       <span><a href={`https://www.vegvesen.no/trafikkdata/start/utforsk?datatype=weekVolume&display=chart&trpids=${trp.id}&from=${fromDate}`}>{this.state.dt.total.volume}</a> ({fromDate}, {Math.round(this.state.dt.total.coverage.percentage)}% dekningsgrad)</span>
     );
